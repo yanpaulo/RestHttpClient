9d17aacd5a685346adebb3d66ea6bceb98a9f597
Yan Paulo <yanpaulo@hotmail.com>
2019-03-18 16:14:27 -0300

    PostByHttpRequestMessage test

--- RestHttpClient.Test/TodoTest.cs
+++ RestHttpClient.Test/TodoTest.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -7,7 +9,7 @@ namespace Yansoft.Rest.Test
 {
     public class TodoTest
     {
-        private RestHttpClient ws = new RestHttpClient
+        private RestHttpClient client = new RestHttpClient
         {
             BaseAddress = new Uri("https://jsonplaceholder.typicode.com")
         };
@@ -16,7 +18,7 @@ namespace Yansoft.Rest.Test
         [Fact]
         public async void GetAll()
         {
-            var todos = await ws.RestGetAsync<List<Todo>>("todos");
+            var todos = await client.RestGetAsync<List<Todo>>("todos");
 
             Assert.Equal(200, todos.Count);
         }
@@ -24,7 +26,7 @@ namespace Yansoft.Rest.Test
         [Fact]
         public async void GetSingle()
         {
-            var todo = await ws.RestGetAsync<Todo>("todos/1");
+            var todo = await client.RestGetAsync<Todo>("todos/1");
 
             Assert.Equal(1, todo.Id);
         }
@@ -33,8 +35,8 @@ namespace Yansoft.Rest.Test
         public async void GetByFullUrlEqualToRelative()
         {
             const string path = "todos/1";
-            var relative = await ws.RestGetAsync<Todo>(path);
-            var full = await ws.RestGetAsync<Todo>($"https://jsonplaceholder.typicode.com/{path}");
+            var relative = await client.RestGetAsync<Todo>(path);
+            var full = await client.RestGetAsync<Todo>($"https://jsonplaceholder.typicode.com/{path}");
 
             Assert.Equal(relative, full);
         }
@@ -48,10 +50,26 @@ namespace Yansoft.Rest.Test
                 Title = "Lorem Ipsum"
             };
 
-            var item = await ws.RestPostAsync<Todo>("todos", todo);
+            var item = await client.RestPostAsync<Todo>("todos", todo);
 
             Assert.Equal(todo.UserId, item.UserId);
             Assert.Equal(todo.Title, item.Title);
+        }
+
+        [Fact]
+        public async void PostByHttpRequestMessage()
+        {
+            var json = "{ \"title\": \"Lorem Ipsum\", \"userId\": 1 }";
+            
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                Content = new StringContent(json, Encoding.UTF8, "application/json"),
+                RequestUri = new Uri("todos", UriKind.Relative)
+            };
+
+            var response = await client.RestSendAsync(request, authRetry: false);
+            Assert.True(response.IsSuccessStatusCode);
         }
     }
 }
