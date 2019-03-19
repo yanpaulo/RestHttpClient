eab44443a1a6ca58a20f94b77fa052f3f0304b19
Alex Knight <alex@aktheknight.co.uk>
2019-03-19 22:10:48 +0100

    Remove anonymous type commits

--- Source/JsonRestConverter.cs
+++ Source/JsonRestConverter.cs
@@ -28,18 +28,6 @@ namespace Yansoft.Rest
             }
         }
 
-        /// <summary>
-        /// Deserialize an object with type given, used for anonymous type objects
-        /// </summary>
-        /// <param name="content">Content to be deserialized</param>
-        /// <param name="type">Type to deserialize to</param>
-        /// <typeparam name="T">Type to deserialize to, inferred from type field</typeparam>
-        /// <returns>Deserialized anonymous string from JSON String</returns>
-        public T Deserialize<T>(string content, T type)
-        {
-            return JsonConvert.DeserializeAnonymousType(content, type, SerializerSettings);
-        }
-
         public virtual string Serialize(object o)
         {
             return JToken.FromObject(o, JsonSerializer).ToString();
